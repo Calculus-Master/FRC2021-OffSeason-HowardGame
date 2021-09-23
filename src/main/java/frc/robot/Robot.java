91600fa069280d5b0ade5559d663a549fdf5b3b1
Saptarshi Mallick <sapmallick@gmail.com>
2021-09-22 20:47:18 -0700

    Added basic buttons, fixed Sim

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,17 +1,38 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.utils.PS4Constants;
 
 public class Robot extends TimedRobot
 {
+    public static final int ELEVATOR_ID = 1;
+
     private WPI_TalonSRX elevator;
+
+    private Joystick joystick;
+
+    private JoystickButton elevatorBottomLevelButton;
+    private JoystickButton elevatorMidLevelButton;
+    private JoystickButton elevatorUpperLevelButton;
 
     @Override
     public void robotInit()
     {
-        this.elevator = new WPI_TalonSRX(1);
+        this.elevator = new WPI_TalonSRX(ELEVATOR_ID);
+        this.joystick = new Joystick(0);
+
+        this.elevatorBottomLevelButton = new JoystickButton(this.joystick, PS4Constants.CROSS.getValue());
+        this.elevatorMidLevelButton = new JoystickButton(this.joystick, PS4Constants.CIRCLE.getValue());
+        this.elevatorUpperLevelButton = new JoystickButton(this.joystick, PS4Constants.TRIANGLE.getValue());
+
+        this.elevatorBottomLevelButton
+                .whenPressed(() -> this.elevator.set(ControlMode.PercentOutput, 0.5))
+                .whenReleased(() -> this.elevator.set(0.0));
     }
 
     @Override
