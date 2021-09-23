0eb6cdf696bbab5201118c763b51d1529f76292f
Saptarshi Mallick <sapmallick@gmail.com>
2021-09-22 19:37:32 -0700

    Added elevator motor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,27 +1,34 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
+public class Robot extends TimedRobot
+{
+    private WPI_TalonSRX elevator;
 
     @Override
-    public void robotInit() {
+    public void robotInit()
+    {
+        this.elevator = new WPI_TalonSRX(1);
     }
 
     @Override
-    public void robotPeriodic() {
+    public void robotPeriodic()
+    {
         // always run the CommandScheduler during periodic
         CommandScheduler.getInstance().run();
     }
 
     @Override
-    public void teleopInit() {
+    public void teleopInit()
+    {
 
     }
 
-    public static Robot win() {
+    public static Robot win()
+    {
         return new Robot();
     }
-
 }
